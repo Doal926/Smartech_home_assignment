84208fbea417646b90f34eb2906011bc5b63b272
Doal926 <doal926@gmail.com>
2024-08-24 20:20:28 +0300

    Fixing metrics

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import logging
+import time
 
 from flask import Flask, Response, abort, jsonify, request
 from flask_httpauth import HTTPBasicAuth
-from prometheus_client import CONTENT_TYPE_LATEST, Counter, Summary, generate_latest
+from prometheus_client import CONTENT_TYPE_LATEST, Counter, Histogram, generate_latest
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from models import User, db
@@ -23,9 +24,13 @@ db.init_app(app)
 
 # Prometheus metrics
 # Initialize Prometheus metrics
-REQUEST_COUNT = Counter("request_count", "Total number of requests")
-REQUEST_LATENCY = Summary(
-    "request_latency_seconds", "Latency of HTTP requests in seconds"
+REQUEST_COUNT = Counter(
+    "http_requests_total", "Total HTTP Requests", ["method", "endpoint", "http_status"]
+)
+REQUEST_LATENCY = Histogram(
+    "http_request_duration_seconds",
+    "HTTP request latency in seconds",
+    ["method", "endpoint", "http_status"],
 )
 
 
@@ -49,6 +54,22 @@ def verify_password(username, password):
     return None
 
 
+# Prometheus middleware to track requests
+@app.before_request
+def before_request():
+    request.start_time = time.time()
+
+
+@app.after_request
+def after_request(response):
+    request_latency = time.time() - request.start_time
+    REQUEST_LATENCY.labels(request.method, request.path, response.status_code).observe(
+        request_latency
+    )
+    REQUEST_COUNT.labels(request.method, request.path, response.status_code).inc()
+    return response
+
+
 # Expose Prometheus metrics
 @app.route("/metrics")
 def metrics():
@@ -59,80 +80,72 @@ def metrics():
 @app.route("/users", methods=["POST"])
 @auth.login_required
 def create_user():
-    with REQUEST_LATENCY.time():
-        data = request.json
-        username = data.get("username")
-        password = data.get("password")
-        # Validate the incoming request data
-        if "username" not in data or "password" not in data:
-            abort(400, "Missing username or password")
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+    # Validate the incoming request data
+    if "username" not in data or "password" not in data:
+        abort(400, "Missing username or password")
 
-        existing_user = User.query.filter_by(username=username).first()
-        if existing_user:
-            return jsonify({"message": "User already exists"}), 409
-        # Hash the user's password for security
-        hashed_password = generate_password_hash(password)
-        new_user = User(username=username, password_hash=hashed_password)
-        # Add the new user to the database
-        db.session.add(new_user)
-        db.session.commit()
-        # Return the created user data with a 201 Created status
-        logger.info(f"User - {new_user.username} was created")
-        REQUEST_COUNT.inc()
-        return jsonify(new_user.to_dict()), 201
+    existing_user = User.query.filter_by(username=username).first()
+    if existing_user:
+        return jsonify({"message": "User already exists"}), 409
+    # Hash the user's password for security
+    hashed_password = generate_password_hash(password)
+    new_user = User(username=username, password_hash=hashed_password)
+    # Add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+    # Return the created user data with a 201 Created status
+    logger.info(f"User - {new_user.username} was created")
+    return jsonify(new_user.to_dict()), 201
 
 
 # Endpoint to retrieve all users
 @app.route("/users", methods=["GET"])
 @auth.login_required
 def get_users():
-    with REQUEST_LATENCY.time():
-        # Query all users from the database
-        users = User.query.all()
-        # Return the list of users as a JSON response
-        REQUEST_COUNT.inc()
-        return jsonify([user.to_dict() for user in users]), 200
+    # Query all users from the database
+    users = User.query.all()
+    # Return the list of users as a JSON response
+    return jsonify([user.to_dict() for user in users]), 200
 
 
 # Endpoint to update an existing user
 @app.route("/users/<int:user_id>", methods=["PUT"])
 @auth.login_required
 def update_user(user_id):
-    with REQUEST_LATENCY.time():
-        user = db.session.get(User, user_id)
-        if not user:
-            abort(404, "User not found")
-        data = request.json
-        # Update the username and/or password if provided in the request
-        if "username" in data:
-            logger.info(f"User - {user.username} was updated to {data['username']}")
-            user.username = data["username"]
-        if "password" in data:
-            logger.info(f"User - {user.username} password was updated")
-            user.password_hash = generate_password_hash(data["password"])
-        db.session.commit()
+    user = db.session.get(User, user_id)
+    if not user:
+        abort(404, "User not found")
+    data = request.json
+    # Update the username and/or password if provided in the request
+    if "username" in data:
+        logger.info(f"User - {user.username} was updated to {data['username']}")
+        user.username = data["username"]
+    if "password" in data:
+        logger.info(f"User - {user.username} password was updated")
+        user.password_hash = generate_password_hash(data["password"])
+    db.session.commit()
 
-        # Return the updated user data
-        REQUEST_COUNT.inc()
-        return jsonify(user.to_dict()), 200
+    # Return the updated user data
+    return jsonify(user.to_dict()), 200
 
 
 # Endpoint to delete a user
 @app.route("/users/<int:user_id>", methods=["DELETE"])
 @auth.login_required
 def delete_user(user_id):
-    with REQUEST_LATENCY.time():
-        user = db.session.get(User, user_id)
-        if not user:
-            abort(404, "User not found")
-        # Delete the user from the database
-        user_name = user.username
-        db.session.delete(user)
-        db.session.commit()
-        logger.info(f"User - {user_name} was deleted")
-        # Return a 204 No Content status
-        REQUEST_COUNT.inc()
-        return "", 204
+    user = db.session.get(User, user_id)
+    if not user:
+        abort(404, "User not found")
+    # Delete the user from the database
+    user_name = user.username
+    db.session.delete(user)
+    db.session.commit()
+    logger.info(f"User - {user_name} was deleted")
+    # Return a 204 No Content status
+    return "", 204
 
 
 # Run the Flask application
